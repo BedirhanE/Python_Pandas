0a353d1f37de3e035301ec19a84adc38c6d1288f
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-10-31 18:17:51 +0300

    update

--- Seaborn.py
+++ Seaborn.py
@@ -1,29 +1,22 @@
 import seaborn as sns
 
-
-
-# Örnek bir veri seti yükle
-data = sns.load_dataset("tips")
-
-# Veri setini görüntüle
-print("tips veri seti :",data)
-
-
 print("-------------------------------------------")
 #Veri görselleştirme yaparak veri çekmme işlemi
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Titanic veri setini yükle
+# Titanic veri setini yükleme işlemi yaptım
 titanic_data = sns.load_dataset("titanic")
 
-# Veri setini özetle (örneğin, yaş ortalamalarını hesapla)
+# Veri setini özetle (örneğin, yaş ortalamalarını hesaplama işlemi)
 age_summary = titanic_data.groupby("class")["age"].mean().reset_index()
 
-# Veriyi görselleştir (örnek olarak bar plot)
+# Veriyi görselleştir (örnek olarak bar plot kütp. ile yaptım.)
 sns.barplot(x="class", y="age", data=age_summary)
 plt.title("Sınıfa Göre Ortalama Yaş")
 plt.xlabel("Sınıf")
 plt.ylabel("Ortalama Yaş")
 plt.show()
+
+
