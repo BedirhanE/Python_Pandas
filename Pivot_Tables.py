138e444e41fb20d7f869db8f8e21e8f83d5db0e4
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-11-07 14:45:32 +0300

    Update Pivot_Tables.py

--- Pivot_Tables.py
+++ Pivot_Tables.py
@@ -1,14 +1,42 @@
-import  pandas as pd
+import pandas as pd
 import seaborn as sns
-titanic=sns.load_dataset("titanic")
-titanic.head()
+import matplotlib.pyplot as plt
+
+# Titanic veri setini yükleme adımı
+titanic = sns.load_dataset("titanic")
+
+# Veri setinin ilk 5 satırını görüntülemek için gerekli kod
 print(titanic.head())
-#sıralama işlemi
-#print(titanic.groupby("sex")["survived"].mean())
-#pivot table oluşturma
-#print(titanic.groupby(["sex","class"])[["survived"]].agreegate("mean").unstack())
 
-#print(titanic.pivot_table("survived",index="sex",columns="class"))
-#print(titanic.age.head())
+# Yaşa göre hayatta kalma oranlarını hesaplama işlemi
+age_survival = titanic.groupby("age")["survived"].mean()
+print("Yaşa göre hayatta kalma:",age_survival)
 
-print(titanic.pivot_table("survived",["sex","age"],"class"))
+# Bu verileri bir çizgi grafiği ile görselleştirin
+plt.figure(figsize=(12, 6))
+age_survival.plot()
+plt.title("Yaşa Göre Hayatta Kalma Oranları")
+plt.xlabel("Yaş")
+plt.ylabel("Hayatta Kalma Oranı")
+plt.grid(True)
+plt.show()
+
+# Yolcu sınıfına göre yaş ve cinsiyete bağlı hayatta kalma oranlarını hesaplayın
+age_class_sex_survival = titanic.pivot_table("survived", index=["sex", "age"], columns="class")
+
+# Bu verileri bir ısı haritası ile görselleştirin
+plt.figure(figsize=(10, 6))
+sns.heatmap(age_class_sex_survival, annot=True, cmap="coolwarm")
+plt.title("Yolcu Sınıfına ve Yaşa Göre Hayatta Kalma Oranları")
+plt.show()
+
+# Cinsiyete göre yolcu sayısını hesaplama
+gender_counts = titanic["sex"].value_counts()
+
+# Bu verileri bir çubuk grafiği ile görselleştirme işlemi
+plt.figure(figsize=(8, 5))
+sns.barplot(x=gender_counts.index, y=gender_counts.values)
+plt.title("Cinsiyete Göre Yolcu Sayısı")
+plt.xlabel("Cinsiyet")
+plt.ylabel("Yolcu Sayısı")
+plt.show()
